709253a30164229cfa7839a7f973a104d2ea5ea1
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2024-09-16 18:07:01 -0300

    IFOOD-0001: Ajustes de estilização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from "./presentation/login/login.component";
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent, title: "iFlow: Bem Vindo de Volta!" }
 ];
