92956ee7f66ba2ad234a3ac88f8d5a4a755406a3
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2024-09-16 18:52:17 -0300

    IFOOD-0001: Criação tela criar-conta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from "./presentation/login/login.component";
+import {CriarContaComponent} from "./presentation/criar-conta/criar-conta.component";
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent, title: "iFlow: Bem Vindo de Volta!" }
+  { path: '', component: LoginComponent, title: "iFlow: Bem Vindo de Volta!" },
+  { path: 'criar-conta', component: CriarContaComponent, title: "iFlow: Crie Sua Conta!"}
 ];
